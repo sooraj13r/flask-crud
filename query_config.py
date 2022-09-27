976d9ae2da7dcfa80b4304c4c820c6e4ca60cf29
Sooraj R <51256389+sooraj13r@users.noreply.github.com>
2022-09-27 10:16:28 +0530

    Delete query_config.py

--- query_config.py
+++
@@ -1,6 +0,0 @@
-
-query_select_all= "SELECT * FROM book ORDER BY id DESC"
-query_select_by_id = "SELECT * FROM book WHERE id = %s"
-query_update = 'UPDATE book SET book_title = %s, book_price = %s WHERE id =%s'
-query_insert = 'INSERT INTO book (book_title, book_price) VALUES(%s,%s)'
-query_delete = 'DELETE FROM book WHERE id = %s'
