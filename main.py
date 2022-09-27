27e9ef554dfa90fa20a18e1f4fe93fe6007a0051
Sooraj R <51256389+sooraj13r@users.noreply.github.com>
2022-09-27 10:13:32 +0530

    upadted

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-from book_service import Book,BookService,Test
-import logging
-from log_config import config_log
+from book_service import Test
 
-config_log()
-app = Flask(__name__)
+main = Flask(__name__)
 api = Api(main)
-
-api.add_resource(BookService,
-                 '/books/<string:book_id>'
-                )
-api.add_resource(Book,
-                 '/books'
-                )
 api.add_resource(Test,
                  '/test'
                 )               
 
 if __name__ == "__main__":
-    app.run(port = 5080, debug =True)
-    logging.info('Host sarted on port:{}'.format(5080))
+    app.run(port = 5000, debug =True)
